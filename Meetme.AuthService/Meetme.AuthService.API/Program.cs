31bab3620ea37ac12a7923463f02ebdc71927777
Mih-Mir <mihailmironov74@gmail.com>
2025-01-07 14:02:38 +0300

    Add auth0 layer

--- Meetme.AuthService/Meetme.AuthService.API/Program.cs
+++ Meetme.AuthService/Meetme.AuthService.API/Program.cs
@@ -1,8 +1,33 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>
+{
+	options.AddDefaultPolicy(builder =>
+		builder.AllowAnyOrigin()
+			.AllowAnyMethod()
+			.AllowAnyHeader());
+});
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+	.AddJwtBearer(options =>
+	{
+		options.Authority = $"https://{builder.Configuration["Auth0:Domain"]}/";
+		options.Audience = builder.Configuration["Auth0:Audience"];
+		options.TokenValidationParameters = new TokenValidationParameters
+		{
+			NameClaimType = ClaimTypes.NameIdentifier
+		};
+	});
+
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
@@ -14,6 +39,9 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseCors();
+
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
